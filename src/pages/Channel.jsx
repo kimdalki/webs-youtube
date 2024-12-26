2ac6d1a63cf12e41aeaed15ecbe0c41ff008c129
drakins <kimdalki5@hanmail.net>
2024-12-26 18:24:17 +0900

    페이지 SEO 작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
-const Channel = () => {
+function Channel() {
     return (
-        <Main>
+        <Main
+            title="유튜브 채널"
+            description="유튜브 채널페이지입니다.">
             Channel
         </Main>
     )
